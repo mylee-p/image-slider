442c0d80a5a39c9ec0bc4d32ad703b0ab26a965d
dev_mylee <leemy0122@gmail.com>
2022-11-18 17:00:09 +0900

    Fourth commit

--- src/js/imageSlider.js
+++ src/js/imageSlider.js
@@ -5,6 +5,10 @@ export default class ImageSlider {
 
   #slideWidth = 0;
 
+  #intervalId;
+
+  #autoPlay = true;
+
   sliderWrapEl;
 
   sliderListEl;
@@ -15,6 +19,8 @@ export default class ImageSlider {
 
   indicatorWrapEl;
 
+  controlWrapEl;
+
   constructor() {
     this.assignElement();
     this.initSliderNumber();
@@ -23,6 +29,7 @@ export default class ImageSlider {
     this.addEvent();
     this.createIndicator();
     this.setIndicator();
+    this.initAutoplay();
   }
 
   assignElement() {
@@ -31,6 +38,11 @@ export default class ImageSlider {
     this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
     this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
     this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
+    this.controlWrapEl = this.sliderWrapEl.querySelector('#control-wrap');
+  }
+
+  initAutoplay() {
+    this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
   }
 
   initSliderNumber() {
@@ -52,6 +64,21 @@ export default class ImageSlider {
       'click',
       this.onClickIndicator.bind(this),
     );
+    this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
+  }
+
+  togglePlay(event) {
+    if (event.target.dataset.status === 'play') {
+      this.#autoPlay = true;
+      this.controlWrapEl.classList.add('paly');
+      this.controlWrapEl.classList.remove('pause');
+      this.initAutoplay();
+    } else if (event.target.dataset.status === 'pause') {
+      this.#autoPlay = false;
+      this.controlWrapEl.classList.remove('paly');
+      this.controlWrapEl.classList.add('pause');
+      clearInterval(this.#intervalId);
+    }
   }
 
   onClickIndicator(event) {
@@ -73,6 +100,12 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
+
     this.setIndicator();
   }
 
@@ -84,6 +117,11 @@ export default class ImageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+
+    if (this.#autoPlay) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+    }
     this.setIndicator();
   }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import '../css/style.css';
 import Slider from './imageSlider';
 
-new Slider();
+// eslint-config-airbnb 룰 때문에 변수가지고 아무것도 하지 않으면
+// 빨간 줄이 뜨는데 현재 이걸로 뭘 하지 않을 것이기 때문에 무시해도 무방하다.
+const slider = new Slider();
