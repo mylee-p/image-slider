3ea2ec97ea321b0a893ca88bf66e198029b55819
dev_mylee <leemy0122@gmail.com>
2022-11-18 13:53:08 +0900

    third commit

--- src/js/imageSlider.js
+++ src/js/imageSlider.js
@@ -13,12 +13,16 @@ export default class ImageSlider {
 
   previousBtnEl;
 
+  indicatorWrapEl;
+
   constructor() {
     this.assignElement();
     this.initSliderNumber();
     this.initSlideWidth();
     this.initSliderListWidth();
     this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
   }
 
   assignElement() {
@@ -26,6 +30,7 @@ export default class ImageSlider {
     this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
     this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
     this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
   }
 
   initSliderNumber() {
@@ -33,7 +38,7 @@ export default class ImageSlider {
   }
 
   initSlideWidth() {
-    this.#slideWidth = this.sliderWrapEl.clientWidth;
+    this.#slideWidth = this.sliderListEl.clientWidth;
   }
 
   initSliderListWidth() {
@@ -43,28 +48,59 @@ export default class ImageSlider {
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
     this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+  }
+
+  onClickIndicator(event) {
+    const indexPosition = parseInt(event.target.dataset.index, 10);
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#slideWidth * this.#currentPosition
+      }px`;
+      this.setIndicator();
+    }
   }
 
   moveToRight() {
     this.#currentPosition += 1;
-
     if (this.#currentPosition === this.#slideNumber) {
       this.#currentPosition = 0;
     }
-
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
   }
 
   moveToLeft() {
     this.#currentPosition -= 1;
-
     if (this.#currentPosition === -1) {
       this.#currentPosition = this.#slideNumber - 1;
     }
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
+  }
+
+  createIndicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#slideNumber; i += 1) {
+      const li = document.createElement('li');
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  setIndicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+    this.indicatorWrapEl
+      .querySelector(`ul li:nth-child(${this.#currentPosition + 1})`)
+      .classList.add('active');
   }
 }
